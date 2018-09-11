b858a37adf4e0693e6462c65ffc0ef358230d032
Sean <34431466+Smwitte27@users.noreply.github.com>
2018-09-11 14:40:25 -0500

    Added AsciiBox Labs

---
+++ AsciiBox.java
@@ -0,0 +1,37 @@
+package output;
+
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name - Sean Witte
+//Date - 09/11/18
+//Class - Period 2
+//Lab  - AsciiBox
+
+public class AsciiBox
+{
+	static String Pluses = ("+++++++++++++++++++++++++ ");
+	static String Aline = ("AAAAAAAAAAAAAAAAAAAAAAAAA ");
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Sean Witte \t 09/11/18 \n\n" );
+		System.out.println(Pluses);
+		System.out.println(Pluses);
+		System.out.println(Pluses);
+		System.out.println(Aline);
+		System.out.println(Aline);
+		System.out.println(Pluses);
+		System.out.println(Pluses);
+		System.out.println(Pluses);
+		System.out.println(Aline);
+		System.out.println(Aline);
+		System.out.println(Pluses);
+		System.out.println(Pluses);
+		System.out.println(Pluses);
+		System.out.println(Aline);
+		System.out.println(Aline);
+		System.out.println(Pluses);
+		System.out.println(Pluses);
+		System.out.println(Pluses);
+
+	}
+}
---
+++ AsciiBox2.java
@@ -0,0 +1,33 @@
+package output;
+
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name - Sean Witte
+//Date - 09/11/18
+//Class - Period 2
+//Lab  - AsciiBox2
+
+public class AsciiBox2
+{
+	static String Pluses = ("+++++++++++++++++++++++++ ");
+	static String Aline = ("AAAAAAAAAAAAAAAAAAAAAAAAA ");
+	static String Gapline = ("+++    \t\t      +++");
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Sean Witte \t 09/11/18 \n\n" );
+		System.out.println(Aline);
+		System.out.println(Pluses);
+		System.out.println(Gapline);
+		System.out.println(Gapline);
+		System.out.println(Gapline);
+		System.out.println(Gapline);
+		System.out.println("+++\tCompsci       +++");
+		System.out.println(Gapline);
+		System.out.println(Gapline);
+		System.out.println(Gapline);
+		System.out.println(Gapline);
+		System.out.println(Aline);
+		System.out.println(Pluses);
+
+	}
+}
